c79d23aaeb6214ff65090e44fca1acc3693e369e
Nick From Normandy <NickFromNormandy@gmail.com>
2013-12-31 14:29:26 -0800

    clean up but still no module

--- ocaml/FromASequenceOfLinesToOccurenceOfWords/line.ml
+++ ocaml/FromASequenceOfLinesToOccurenceOfWords/line.ml
@@ -1,12 +1,13 @@
 
 (* #load "str.cma";; *)
  
-(** The goal is to:
-  - read a text file
-  - parse it and read each word.
+(** The module is to:
+  - create a list of words from a text file
+  - create a map that expresses the occurence of each word
   - a word is a sequence of letters 
   - print a list of occurence of each word
  *)
+
 
 module StringMap = Map.Make(String);;
 module SetOfInt = Set.Make(struct type t = int let compare = compare end);;
@@ -22,35 +23,38 @@ let find_max my_map =
   !max_values;;
 
 
-(** If the word does not belong to the map's domain then
-    add an association (word, 1)
-    else add 1 to the counter associated with the word.
-*)        
+(** Helper function: If the word does not belong to the map's domain then
+    add an association (word, 1) to the map
+    else add 1 to the counter associated with the existing word.*)        
 let compute_new_map my_key my_map = 
   if (StringMap.mem my_key my_map) then 
     (let value = (StringMap.find my_key my_map) in 
      StringMap.add my_key (value+1) my_map)
   else (StringMap.add my_key 1 my_map);;
 
+
 (** Create a map from a list of words
-   each words that belong to the list will be part of the domain of the map
-   the number of occurence the word in the list will be part of the range of the map.
-   the map will have a word and its number of occurence
+   - each words that belong to the list will be part of the domain of the map
+   - the number of occurence the word in the list will be part of the range of the map.
+   - the map will have a word and its number of occurence
 *)
-let my_word_to_count my_list_of_words =
-  let rec compute_word_to_count my_list_of_words my_map =
+let create_a_map_word_occurence_from_listofwords my_list_of_words =
+  let rec compute_word_to_occurence my_list_of_words my_map =
     match my_list_of_words with
       [] -> my_map
-    | head::tail -> compute_word_to_count tail (compute_new_map head my_map)  in
-  compute_word_to_count my_list_of_words StringMap.empty;;
+    | 
+      head::tail -> compute_word_to_occurence tail (compute_new_map head my_map)  in
+  compute_word_to_occurence my_list_of_words StringMap.empty;;
 
 
-(** Read a file denoted by the variable filename
-   return a list of words
-   each word belong to one of the lines.
-   a word is a sequence of letter (lower or upper case)
- *)  
-let read_file filename = 
+(** Read a file d by filename
+   - return a list of words
+   - each word belongs to excatly one line 
+   - there is duplicated word in the list
+   - a word is a sequence of letters 
+   - a letter is either lower or upper case
+*)  
+let create_a_list_of_words_from_a_text_file filename = 
   let lines = ref [] in
   let chan = open_in filename in
   try
@@ -64,20 +68,23 @@ let read_file filename =
     close_in chan;
     List.rev !lines ;;
 
-(** This function is not used  
- *)
+(** This function is not used  *)
 let rec print_my_list myStringMap myString =
-  match myString with
+  match 
+    myString 
+  with
     [] -> print_string "The list is empty"; myStringMap
-  | head::tail -> print_string head; print_string "...\n"; print_my_list (StringMap.add "titi" "toto" myStringMap) tail;;
+  |
+    head::tail -> print_string head; print_string "...\n"; print_my_list (StringMap.add "titi" "toto" myStringMap) tail;;
   
-(** Print a list of words to the console 
- *)
+(** Print a list of words to the console  *)
 let print_my_list_of_words my_list = 
   print_string "[";
   let counter = ref 0 in
   let rec print_it my_list_of_words =
-    match my_list_of_words with 
+    match 
+      my_list_of_words 
+    with 
       [] -> print_string "\n"
     | head::tail -> print_string head;
 		    print_string ",";
@@ -87,28 +94,32 @@ let print_my_list_of_words my_list =
   in
   print_it my_list;;
 
-(** Print word to count *)
-let rec print_word_to_count my_map =
-  print_string "word to count s";
-  let 
-    print_key_value my_key my_value = print_string "coucou:";
+(** Print the map word to occurence *)
+let rec print_word_to_occurence my_map =
+  print_string "word to occurence:";
+  let print_key_value my_key my_value = 
+    print_string "association word->occurence:";
     print_string my_key;print_string "->";
     (print_string (string_of_int(my_value)));
     print_string "\n"; 
   in
   StringMap.iter print_key_value my_map;;
   
-(** Helper function to print an int value used with iter *)
-let print_myint my_int = 
-  print_int my_int; print_string ",";;
 
-(** print each int value that belong to the passed-in set*)
+(** Print each int value that belongs to the passed-in set*)
 let print_my_set my_set =
+  (** Helper function to print an int value used with iter *)
+  let print_myint my_int = 
+    print_int my_int; print_string "," in
   SetOfInt.iter print_myint my_set;;
 
-(**  Create the list of words*)
-let my_list_of_words = read_file "toto.txt" in
+(** Create the list of words and then create the map that will mach each word with its occurence*)
+let my_list_of_words = create_a_list_of_words_from_a_text_file "toto.txt" in
+    print_string "Length of the list of words:";
     print_int (List.length(my_list_of_words));
+    print_string "\n";
     print_my_list_of_words my_list_of_words;
-    let myWordToCount = (my_word_to_count my_list_of_words) in
-    print_word_to_count myWordToCount;print_my_set (find_max myWordToCount);;
+    let myMapWordToOccurence = (create_a_map_word_occurence_from_listofwords my_list_of_words) in
+    print_word_to_occurence myMapWordToOccurence;
+    
+
