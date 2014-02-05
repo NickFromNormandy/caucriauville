885bf6d1d24f425ab05cea748a25380e8a073dc4
Nick From Normandy <NickFromNormandy@gmail.com>
2014-02-05 13:40:46 -0800

    it works for knapsack - brute force

--- c++/Knapsack.cpp
+++ c++/Knapsack.cpp
@@ -22,19 +22,19 @@ public:
   int getWeight() { return m_weight; };
   int getValue()  { return m_value; };
   std::string getName() { return m_name; };
-  void print() { std::cout << "item:" << m_name << " value:" << m_value << " weight:" << m_weight << "\n";};
+  void print() { std::cout << "item:" << m_name << " value: $" << m_value << " weight:" << m_weight << "kg\n";};
 };
 
 class SetOfItems : public std::vector<Item> 
 {
 public:
-  
+  void print(void) { std::cout << "print all items\n"; for(int i=0;i<size();i++) this->operator[](i).print(); }
 };
 
 class BackPack 
 {
 public:
-  BackPack(int weight, SetOfItems myItems) : m_weight(weight),  m_items(myItems), m_value(0) {};
+  BackPack(int weight, SetOfItems &myItems) : m_weight(weight),  m_items(myItems), m_value(0) {};
   void ComputeBestBag();
   void PrintChoice();
 private:
@@ -47,15 +47,19 @@ private:
 
 void BackPack::ComputeOneCase(unsigned int members, int& weight, int& value)
 {
+  weight = 0;
+  value = 0;
   for(int i=0;i < m_items.size() && i<sizeof(members)*8;i++)
   {
     unsigned int bitmask = (1 << i);
     
     if (members & bitmask )
     {
-      std::cout << members << "," << bitmask << "\n";
-      weight = weight + m_items[i].getWeight();
+      
+      weight = weight + m_items.operator[](i).getWeight();
       value = value + m_items[i].getValue();
+      
+      m_items.operator[](i).print();
     }
   }
 }
@@ -64,14 +68,17 @@ void BackPack::ComputeBestBag()
 {
   int NumberOfItems = m_items.size();
   int choices = (1 << NumberOfItems);
-  int weight, value;
+  m_value =0;
+  m_case=0;
 
   for(int i=0;i<choices;i++)
   {
-
+    int weight=0, value=0;
     ComputeOneCase(i, weight, value);
+    std::cout << "\tFor once case: Choices:" << i << "weight:" << weight << "value:" << value << "\n";
     if (weight <= m_weight && value > m_value)
     {
+      //std::cout << "Choices:" << i << "weight:" << weight << "value:" << value << "\n";
       m_value = value;
       m_case = i;
     }
@@ -82,12 +89,18 @@ void BackPack::ComputeBestBag()
 void BackPack::PrintChoice()
 {
   std::cout << "There was " << m_items.size() << " items\n";
+  std::cout << "Max weight " << m_weight << "\n";
+  std::cout << "Case " << m_case << "\n";
+
+  //  std::cout << "Max weight " << m_weight << "\n";
+
   for(int i=0;i< m_items.size() && i<sizeof(m_case)*8;i++)
   {
     unsigned int bitmask = (1 << i);
 
     if (m_case & bitmask)
     {
+      std::cout << "item nth:" << i << "\n";
       m_items[i].print();
     }
 
@@ -98,7 +111,7 @@ void BackPack::PrintChoice()
 main()
 {
   SetOfItems myVector;
-  myVector.push_back(Item("Gray", 1, 2));
+  myVector.push_back( Item("Gray", 1, 2));
   myVector.push_back(Item("Orange", 1, 1));
   myVector.push_back(Item("Blue", 2, 2));
   myVector.push_back(Item("Yellow", 4, 10));
@@ -106,4 +119,5 @@ main()
   BackPack myBackPack(15, myVector);
   myBackPack.ComputeBestBag();  
   myBackPack.PrintChoice();
+  myVector.print();
 }
